801ef133d48a18c82575e1885b82f35260805651
Philipp Sauter <philipp.sauter@baobabsoluciones.es>
2019-11-04 17:53:20 +0100

    added callbacks via function pointers between _gowraplp.dll and the built go application with build_windows.go; compiles and runs but is not tested

--- ortools/linear_solver/go/cgo_externals.cc
+++ ortools/linear_solver/go/cgo_externals.cc
@@ -1,17 +1,25 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-void crosscall2(void (*fn)(void *, int), void *, int) {
 
+__declspec(dllexport) void (*crosscall2_ptr)(void (*fn)(void *, int), void *, int);
+void crosscall2(void (*fn)(void *, int), void * void_ptr, int someInt) {
+	crosscall2_ptr(fn, void_ptr, someInt); 
 }
+
+__declspec(dllexport) char* (*_cgo_topofstack_ptr)(void);
 char* _cgo_topofstack(void) {
-    return "";
+    return _cgo_topofstack_ptr();
 }
-void _cgo_allocate(void *, int) {
 
+__declspec(dllexport) void (*_cgo_allocate_ptr)(void *, int);
+void _cgo_allocate(void * void_ptr, int someInt) {
+	_cgo_allocate_ptr(void_ptr, someInt);
 }
-void _cgo_panic(void *, int) {
-    
+
+__declspec(dllexport) void (*_cgo_panic_ptr)(void *, int);
+void _cgo_panic(void * void_ptr, int someInt) {
+    	_cgo_panic_ptr(void_ptr, someInt);
 }
 #ifdef __cplusplus
 }
